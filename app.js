334d26431ddc06bf7b87d276f0b3b21dbfb0c10f
Varun J <varunj1998@gmail.com>
2021-02-21 16:33:54 +0530

    added search feature and timestamps

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
 const mongoose = require("mongoose");
+const moment = require("moment");
 
 const homeStartingContent = "Hello! Welcome to my blog. I like to write about something with the themes of life, the universe and everything. I try to pack in analogies so it helps with visualising some wild theories I think of. Hope you have fun reading. Enjoy!";
 const aboutContent = "Hello! My name is Varun Janardhanan. I am a budding web developer. I am passionate about making stuff. I am also keen to develop games as a hobby. I draw my own art and sometimes write about stuff in general.";
@@ -18,14 +19,17 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 const postSchema = new mongoose.Schema({
   title: String,
+  tags: String,
   contentHTML: String,
-  contentText: String
+  contentText: String,
+  timeStamp: String,
+  author: String
 });
 const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req, res) {
   Post.find({}, function(err, posts) {
-    res.render("home", {title: "Home", startingContent: homeStartingContent, postList: posts});
+    res.render("home", {title: "Home", startingContent: homeStartingContent, postList: posts, moment: moment, formRoute: "/search", linkRoute: "/posts", linkAction: "Read more"});
   });
 });
 
@@ -40,20 +44,23 @@ app.get("/contact", function(req, res) {
 app.get("/posts/:postId", function(req, res) {
   const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId}, function(err, post) {
-    if(!err) {res.render("post", {heading: post.title, content: post.contentHTML});}
+    if(!err) {res.render("post", {post: post, moment: moment});}
   });
 });
 
 app.get("/compose", function(req, res) {
-  res.render("compose", {postId: "", title: "", content: ""});
+  res.render("compose", {postId: "", title: "", tags: "", content: ""});
 });
 
 app.post("/compose", function(req, res) {
   if(req.body.Id == "") {
     const post = new Post({
       title: req.body.Title,
+      tags: req.body.Tags,
       contentHTML: req.body.postContentHTML,
-      contentText: req.body.postContentText
+      contentText: req.body.postContentText,
+      timeStamp: moment().format("YYYYMMDDHHmmss"),
+      author: "Varun"
     });
     post.save(function(err) {
       if(!err) {res.redirect("/");}
@@ -64,6 +71,7 @@ app.post("/compose", function(req, res) {
       { _id: req.body.Id },
       {
         title: req.body.Title,
+        tags: req.body.Tags,
         contentHTML: req.body.postContentHTML,
         contentText: req.body.postContentText
       },
@@ -81,16 +89,66 @@ app.post("/compose", function(req, res) {
 
 app.get("/edit", function(req, res) {
   Post.find({}, function(err, posts) {
-    res.render("edit", {postList: posts});
+    res.render("home", {title: "Editing page", startingContent: "Click on edit to edit a post", postList: posts, moment: moment, formRoute: "/edit/search", linkRoute: "/edit", linkAction: "Edit"});
   });
 });
 
 app.get("/edit/:postId", function(req, res) {
   Post.findById(req.params.postId, function(err, post) {
-    res.render("compose", {postId: post._id, title: post.title, content: post.contentHTML});
+    res.render("compose", {postId: post._id, title: post.title, tags: post.tags, content: post.contentHTML});
   });
 });
 
+app.get("/search/:searchItem", function(req, res) {
+  let searchItem = req.params.searchItem;
+  let queriedPosts = [];
+  let queriedTags = searchItem.match(/\w+/g);
+  Post.find({}, function(err, posts) {
+    posts.forEach(function(post) {
+
+      if(searchItem.includes("[")) {
+        for(tag=0; tag<queriedTags.length; tag++) {
+          if(post.tags.toLowerCase().includes(queriedTags[tag].toLowerCase())) { if(tag==queriedTags.length-1) { queriedPosts.push(post); } }
+          else { break; }
+        }
+      }
+
+      else {
+        if(post.title.toLowerCase().includes(searchItem.toLowerCase())) { queriedPosts.push(post); }
+      }
+    });
+
+    res.render("home", {title: "Search results for:", startingContent: searchItem, postList: queriedPosts, moment: moment, formRoute: "/search", linkRoute: "/posts", linkAction: "Read more"});
+  });
+});
+
+app.post("/search", function(req, res) {
+
+  res.redirect("/search/"+req.body.search);
+});
+
+app.post("/edit/search", function(req, res) {
+  let searchItem = req.body.search;
+  let queriedPosts = [];
+  let queriedTags = searchItem.match(/\w+/g);
+  Post.find({}, function(err, posts) {
+    posts.forEach(function(post) {
+
+      if(searchItem.includes("[")) {
+        for(tag=0; tag<queriedTags.length; tag++) {
+          if(post.tags.toLowerCase().includes(queriedTags[tag].toLowerCase())) { if(tag==queriedTags.length-1) { queriedPosts.push(post); } }
+          else { break; }
+        }
+      }
+
+      else {
+        if(post.title.toLowerCase().includes(searchItem.toLowerCase())) { queriedPosts.push(post); }
+      }
+    });
+
+    res.render("home", {title: "Editing page", startingContent: "Click on edit to edit a post", postList: queriedPosts, moment: moment, formRoute: "/edit/search", linkRoute: "/edit", linkAction: "Edit"});
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
